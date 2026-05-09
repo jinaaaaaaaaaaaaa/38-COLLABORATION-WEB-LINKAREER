71dc7d76165a7a7a7aa86dc415a335f22b9cd6fb
jiwonsudo <wondev112@gmail.com>
2026-05-10 05:06:51 +0900

    chore: ESLint 컨벤션 규칙 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,12 @@
 import js from '@eslint/js';
+import vanillaExtract from '@antebudimir/eslint-plugin-vanilla-extract';
+import react from '@eslint-react/eslint-plugin';
+import stylistic from '@stylistic/eslint-plugin';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import unusedImports from 'eslint-plugin-unused-imports';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
@@ -12,13 +16,49 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+    plugins: {
+      '@eslint-react': react,
+      '@stylistic': stylistic,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      '@eslint-react/no-array-index-key': 'warn',
+      '@stylistic/jsx-pascal-case': 'error',
+      '@stylistic/jsx-self-closing-comp': [
+        'error',
+        {
+          component: true,
+          html: true,
+        },
+      ],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          vars: 'all',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.css.ts'],
+    extends: [vanillaExtract.configs.recommended],
   },
   eslintConfigPrettier,
 ]);
