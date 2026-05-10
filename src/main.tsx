d38a7633619ce59be9f809d12efe3ea7c9554a2d
Soy <da5868@naver.com>
2026-05-10 18:19:18 +0900

    chore: dev 환경에서만 tanstack query tool 노출하도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>,
 );
