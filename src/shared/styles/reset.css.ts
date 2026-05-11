6339ba3ceee47b64d0350f80e144bb14d536bdc9
jiwonsudo <wondev112@gmail.com>
2026-05-11 16:24:38 +0900

    style: meyer-reset reset.css.ts에 적용

--- src/shared/styles/reset.css.ts
+++ src/shared/styles/reset.css.ts
@@ -1,43 +1,54 @@
 import { globalStyle } from '@vanilla-extract/css';
 
-globalStyle('*, *::before, *::after', {
-  boxSizing: 'border-box',
-});
+globalStyle(
+  `html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video`,
+  {
+    margin: 0,
+    border: 0,
+    padding: 0,
+    verticalAlign: 'baseline',
+    font: 'inherit',
+    fontSize: '100%',
+  },
+);
 
-globalStyle('html', {
-  fontSize: '62.5%',
-});
-
-globalStyle('html, body, #root', {
-  minHeight: '100%',
-});
+globalStyle(
+  `article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section`,
+  {
+    display: 'block',
+  },
+);
 
 globalStyle('body', {
-  margin: 0,
+  lineHeight: 1,
 });
 
-globalStyle('button, input, textarea, select', {
-  font: 'inherit',
-});
-
-globalStyle('button, input', {
-  border: 0,
-  background: 'none',
-  cursor: 'pointer',
-});
-
-globalStyle('a', {
-  textDecoration: 'none',
-  color: 'inherit',
-});
-
-globalStyle('ul, ol', {
-  margin: 0,
-  padding: 0,
+globalStyle('ol, ul', {
   listStyle: 'none',
 });
 
-globalStyle('img, picture, video, canvas, svg', {
-  display: 'block',
-  maxWidth: '100%',
+globalStyle('blockquote, q', {
+  quotes: 'none',
+});
+
+globalStyle('blockquote::before, blockquote::after, q::before, q::after', {
+  content: "''",
+});
+
+globalStyle('table', {
+  borderCollapse: 'collapse',
+  borderSpacing: 0,
 });
