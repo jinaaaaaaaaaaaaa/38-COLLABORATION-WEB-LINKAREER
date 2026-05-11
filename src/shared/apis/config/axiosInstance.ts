55a3bc19a56a735db28dbd73d1a3eb88e21b9847
Soy <da5868@naver.com>
2026-05-11 15:13:09 +0900

    chore: axios instance error 객체로 타입

--- src/shared/apis/config/axiosInstance.ts
+++ src/shared/apis/config/axiosInstance.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -12,8 +12,8 @@ axiosInstance.interceptors.response.use(
   (response) => {
     return response;
   },
-  (error) => {
-    return Promise.reject(new Error(String(error)));
+  (error: AxiosError) => {
+    return Promise.reject(error);
   },
 );
 
