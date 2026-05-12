e627a5d0d4ad0ac3db61663d9decf341fdee4398
Jina <jinatye@naver.com>
2026-05-13 02:03:58 +0900

    fix: 테스트 코드 제거

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,24 +1,7 @@
-import {
-  HOME_TABLIST,
-  MENTO_CARTEGORY_TABLIST,
-  MENTO_MAIN_TABLIST,
-  RECRUIT_DETAIL_TABLIST,
-} from '@constants/tabList';
-
-import TabBar from '@components/tabBar/TabBar';
-
 const HomePage = () => {
   return (
     <>
       <h1>홈페이지</h1>
-      <TabBar tabList={HOME_TABLIST} ariaLabel="홈 탭" />
-      <TabBar
-        tabList={RECRUIT_DETAIL_TABLIST}
-        defaultValue="detail"
-        ariaLabel="홈 탭"
-      />
-      <TabBar tabList={MENTO_MAIN_TABLIST} ariaLabel="홈 탭" />
-      <TabBar tabList={MENTO_CARTEGORY_TABLIST} ariaLabel="홈 탭" />
     </>
   );
 };
