674131ba8dac801d84b7d564dfb72fbca41392e9
Soy <da5868@naver.com>
2026-05-13 16:13:29 +0900

    test: 테스트 코드 제거

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,36 +1,7 @@
-import Chip from '@components/chip/Chip';
-
 const HomePage = () => {
   return (
     <>
       <h1>홈페이지</h1>
-      {/* 1. 기본 상태별 비교 (Icon X) */}
-      <section style={{ margin: '2rem 1rem' }}>
-        <div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
-          <Chip variant="default">텍스트</Chip>
-          <Chip variant="filled">텍스트</Chip>
-          <Chip variant="outline">텍스트</Chip>
-          <Chip variant="outlineFilled">텍스트</Chip>
-        </div>
-      </section>
-
-      {/* 2. 아이콘 포함 상태별 비교 (Icon O) */}
-      <section style={{ margin: '0rem 1rem' }}>
-        <div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
-          <Chip variant="default" rightIcon>
-            텍스트
-          </Chip>
-          <Chip variant="filled" rightIcon>
-            텍스트
-          </Chip>
-          <Chip variant="outline" rightIcon>
-            텍스트
-          </Chip>
-          <Chip variant="outlineFilled" rightIcon>
-            마케팅
-          </Chip>
-        </div>
-      </section>
     </>
   );
 };
