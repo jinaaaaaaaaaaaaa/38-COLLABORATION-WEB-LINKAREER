ed850b8cf298c34cd312e13c90179da49757f340
Soy <da5868@naver.com>
2026-05-14 21:48:27 +0900

    feat: 선택 직무 카드 구현 추가

--- src/pages/recruit/components/bottomSheet/FilterSelector.css.ts
+++ src/pages/recruit/components/bottomSheet/FilterSelector.css.ts
@@ -1,10 +1,17 @@
-import { style } from '@vanilla-extract/css';
+import { style, styleVariants } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
 import { vars } from '@styles/theme.css';
 import { typography } from '@styles/tokens/typography';
 
 export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '100%',
+});
+
+export const filterContainer = style({
   display: 'flex',
   alignItems: 'center',
   width: '100%',
@@ -40,15 +47,22 @@ export const tab = recipe({
   },
 });
 
-export const count = style({
-  flexShrink: '0',
-  borderRadius: '50%',
-  backgroundColor: vars.color.blue500,
-  width: '1.6rem',
-  height: '1.6rem',
-  textAlign: 'center',
-  color: vars.color.white,
-  ...typography.caption_m_10,
+export const count = styleVariants({
+  ellipse: {
+    flexShrink: '0',
+    borderRadius: '50%',
+    backgroundColor: vars.color.blue500,
+    width: '1.6rem',
+    height: '1.6rem',
+    textAlign: 'center',
+    color: vars.color.white,
+    ...typography.caption_m_10,
+  },
+  bold: {
+    ...typography.body_b_16,
+    marginLeft: '0.8rem',
+    color: vars.color.blue500,
+  },
 });
 
 export const optionContainer = style({
@@ -85,6 +99,35 @@ export const option = recipe({
         color: vars.color.blue500,
       },
       false: { padding: '1.6rem 0 1.6rem 5.2rem' },
+    },
+  },
+});
+
+export const selectedContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.8rem',
+  backgroundColor: vars.color.white,
+  padding: '1.6rem 0 1.6rem 2rem',
+  width: '100%',
+  height: '9rem',
+});
+
+export const text = style({
+  ...typography.body_b_16,
+  color: vars.color.gray900,
+});
+
+export const tagList = style({
+  display: 'flex',
+  gap: '0.8rem',
+  width: '35.5rem',
+  overflowX: 'auto',
+  whiteSpace: 'nowrap',
+
+  selectors: {
+    '&::-webkit-scrollbar': {
+      display: 'none',
     },
   },
 });
--- src/pages/recruit/components/bottomSheet/FilterSelector.tsx
+++ src/pages/recruit/components/bottomSheet/FilterSelector.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import SvgIcCheckBlue500 from '@assets/svg/IcCheckBlue500';
+import Tag from '@components/tag/Tag';
 import {
   FILTER_OPTIONS,
   FILTER_TABS,
@@ -46,43 +47,73 @@ const FilterSelector = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.tabContainer}>
-        {FILTER_TABS.map((tab) => {
-          const isActive = activeTab === tab.key;
-          const count = getSelectedCount(tab.key);
+      <div className={styles.filterContainer}>
+        {/* 필터 카테고리 */}
+        <div className={styles.tabContainer}>
+          {FILTER_TABS.map((tab) => {
+            const isActive = activeTab === tab.key;
+            const count = getSelectedCount(tab.key);
 
-          return (
-            <button
-              key={tab.key}
-              type="button"
-              onClick={() => setActiveTab(tab.key)}
-              className={styles.tab({ isSelected: isActive })}
-            >
-              <span>{tab.label}</span>
-              {count > 0 && <span className={styles.count}>{count}</span>}
-            </button>
-          );
-        })}
-      </div>
-      <div className={styles.optionContainer}>
-        {currentOptions.map((option) => {
-          const selected = isSelected(option);
+            return (
+              <button
+                key={tab.key}
+                type="button"
+                onClick={() => setActiveTab(tab.key)}
+                className={styles.tab({ isSelected: isActive })}
+              >
+                <span>{tab.label}</span>
+                {count > 0 && (
+                  <span className={styles.count['ellipse']}>{count}</span>
+                )}
+              </button>
+            );
+          })}
+        </div>
 
-          return (
-            <button
-              key={option}
-              type="button"
-              className={styles.option({ isSelected: selected })}
-              onClick={() => handleOptionClick(option)}
-            >
-              {selected && (
-                <SvgIcCheckBlue500 width={'1.6rem'} height={'1.6rem'} />
-              )}
-              {option}
-            </button>
-          );
-        })}
+        {/* 필터 옵션 */}
+        <div className={styles.optionContainer}>
+          {currentOptions.map((option) => {
+            const selected = isSelected(option);
+
+            return (
+              <button
+                key={option}
+                type="button"
+                className={styles.option({ isSelected: selected })}
+                onClick={() => handleOptionClick(option)}
+              >
+                {selected && (
+                  <SvgIcCheckBlue500 width={'1.6rem'} height={'1.6rem'} />
+                )}
+                {option}
+              </button>
+            );
+          })}
+        </div>
       </div>
+
+      {/* 선택한 직무 카드 */}
+      {selectedOptions.length > 0 && (
+        <div className={styles.selectedContainer}>
+          <span className={styles.text}>
+            선택한 직무{' '}
+            <span className={styles.count['bold']}>
+              {selectedOptions.length}
+            </span>
+          </span>
+          <div className={styles.tagList}>
+            {selectedOptions.map((option) => (
+              <Tag
+                key={option}
+                text={`${option}`}
+                color="blue"
+                size="withIcon"
+                onRemove={() => handleOptionClick(option)}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
