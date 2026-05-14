6d5aa7039885853183e640e066e6f9cce08379cf
yumin-kim4757 <rladbals0211@gmail.com>
2026-05-15 04:01:00 +0900

    fix: BNB 공통 레이아웃 lint 오류 수정

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,9 +1,8 @@
 import { Outlet } from 'react-router';
 
+import BottomNavigationBar from '@components/bottomNavigationBar/BottomNavigationBar';
 import Footer from '@components/footer/Footer';
 import GlobalNavigationBar from '@components/globalNavigationBar/GlobalNavigationBar';
-
-import BottomNavigationBar from '@components/bottomNavigationBar/BottomNavigationBar';
 
 
 const AppLayout = () => {
