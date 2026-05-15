608e62da4c3d068c0cccbe626b5b754458fa5206
Jina <jinatye@naver.com>
2026-05-15 19:22:50 +0900

    chore: 글쓰기 button에 aria-label 추가

--- src/pages/mento/components/writeButton/WriteButton.tsx
+++ src/pages/mento/components/writeButton/WriteButton.tsx
@@ -4,7 +4,7 @@ import * as styles from './WriteButton.css';
 
 const WriteButton = () => {
   return (
-    <button type="button" className={styles.writeButton}>
+    <button type="button" className={styles.writeButton} aria-label="글쓰기">
       <SvgIcWrite width="2.8rem" height="2.8rem" />
     </button>
   );
