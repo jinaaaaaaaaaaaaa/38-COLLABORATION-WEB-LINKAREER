e15b67c6e1ac1287b286a7b885c3ad4127e82ee0
yumin-kim4757 <rladbals0211@gmail.com>
2026-05-18 01:56:20 +0900

    style: 홈 채팅 카드 스타일 구현

---
+++ src/pages/home/components/homeChatCard/HomeChatCard.css.ts
@@ -0,0 +1,97 @@
+import { typography } from '@styles/tokens/typography';
+import { style } from '@vanilla-extract/css';
+import { vars } from '@styles/theme.css';
+
+export const card = style({
+  display: 'flex',
+  width: '13.6rem',
+  height: '21rem',
+  padding: '0.8rem',
+  alignItems: 'flex-start',
+  gap: '1rem',
+  borderRadius: '1.2rem',
+  border: '1px solid ${vars.color.gray300}',
+});
+
+export const innerBox = style({
+  display: 'flex',
+  width: '12rem',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '1.6rem',
+  flexShrink: 0,
+});
+
+export const topBox = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '0.8rem',
+  alignSelf: 'stretch',
+});
+
+export const imageBox = style({
+  height: '7.3rem',
+  alignSelf: 'stretch',
+  borderRadius: '0.8rem',
+});
+
+export const image = style({
+  display: 'block',
+  objectFit: 'cover',
+  width: '100%',
+  height: '100%',
+});
+
+export const textBox = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '0.2rem',
+  alignSelf: 'stretch',
+});
+
+export const statusBox = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.2rem',
+  alignSelf: 'stretch',
+});
+
+export const statusIcon = style({
+  width: '1.6rem',
+  height: '1.6rem',
+});
+
+export const statusText = style({
+  ...typography.caption_b_12,
+  color: vars.color.blue500,
+});
+
+export const companyName = style({
+  ...typography.body_b_14,
+  color: vars.color.gray900,
+});
+
+export const participantCount = style({
+  ...typography.caption_r_12,
+  color: vars.color.gray700,
+});
+
+export const buttonBox = style({
+  display: 'flex',
+  width: '12rem',
+  height: '3.6rem',
+  padding: '0.8rem 1.2rem',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '1rem',
+  borderRadius: '0.8rem',
+  border: '1px solid ${vars.color.blue500}',
+});
+
+export const buttonText = style({
+  textAlign: 'center',
+  color: vars.color.blue500,
+  ...typography.caption_m_12,
+});
