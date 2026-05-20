f9df7766c0cf77b07256edb0e0d2da5933fe624d
Jina <jinatye@naver.com>
2026-05-20 14:54:52 +0900

    style: centerBanner 위치 수정

--- src/pages/mento/components/mentoPostList/MentoPostList.tsx
+++ src/pages/mento/components/mentoPostList/MentoPostList.tsx
@@ -1,11 +1,13 @@
+import { Fragment } from 'react';
+
 import MentoCenterBanner from '@pages/mento/components/mentoBanner/mentoCenterBanner/MentoCenterBanner';
+import { CENTER_BANNER_NORMAL_POST_INDEX } from '@pages/mento/constants/banner';
 import { CENTER_AD_DATA } from '@pages/mento/mocks/centerAdData';
 import type { Post } from '@pages/mento/types/post';
 
 import MentoListItem from './mentoListItem/MentoListItem';
 
 import * as styles from './MentoPostList.css';
-
 interface MentoPostListProps {
   postList: Post[];
   isPending: boolean;
@@ -25,14 +27,23 @@ const MentoPostList = ({
     return <div>게시글을 불러오지 못했습니다.</div>;
   }
 
+  const centerBannerTargetPostId = postList.filter((post) => !post.isNotice)[
+    CENTER_BANNER_NORMAL_POST_INDEX
+  ]?.id;
+
   return (
     <ul className={styles.postList}>
       {postList.map((post) => (
-        <MentoListItem key={post.id} post={post} />
+        <Fragment key={post.id}>
+          <MentoListItem post={post} />
+
+          {post.id === centerBannerTargetPostId && (
+            <li>
+              <MentoCenterBanner ad={CENTER_AD_DATA} />
+            </li>
+          )}
+        </Fragment>
       ))}
-      <li>
-        <MentoCenterBanner ad={CENTER_AD_DATA} />
-      </li>
     </ul>
   );
 };
